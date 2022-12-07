f5c2fcb1c7587dd9a0306e7523d6973f770d30a8
umeransari7424 <uf143137@gmail.com>
2022-12-07 17:51:03 +0500

    Get Random data

--- Components/Elements/Shuffle.jsx
+++ Components/Elements/Shuffle.jsx
@@ -1,87 +1,66 @@
 import React, { useState } from "react";
+import card from "../layouts/Data.json";
 
 const Shuffle = () => {
-  const [data, setData] = useState('');
-  const arr1 = [1, 2, 3, 4, 5,6];
-  
+  const [data, setData] = useState("");
+  const[imag,setImag] = useState("");
+  const[id,setId] = useState("");
+  // const arr1 = [1, 2, 3, 4, 5, 6];
+
   const myFunction = () => {
-    const random_data = arr1[Math.floor(Math.random() * arr1.length)];
-    setData(random_data)
+    const random_data = card[Math.floor(Math.random() * card.length)];
+    // console.log(random_data.title);
+    setImag(random_data.img);
+    setId(random_data.id)
+    setData(random_data.title);
   };
-  
+
   return (
     <div className="shuffle">
-      <div className="container text-center py-5">
-        <div className="row text-center">
+      <div className="container text-center">
+        <div className="row  mt-5">
+          {card.map((items) => (
             <div className="col-md-2">
-            <div className="card text-center">
-                <div>
-                    <h1>Card</h1>
-                    <h1>1</h1>
-                </div>
+              {" "}
+              <div className="card px-2 py-3">
+                {" "}
+                <img src={items.img} className="card-img-top img-fluid" alt="..." width={20}/>{" "}
+                <div className="card-body">
+                  {" "}
+                  <h5 className="card-title">Card {items.id}</h5>{" "}
+                  {/* <p className="card-text">{items.text}</p>{" "} */}
+                </div>{" "}
+              </div>{" "}
+            </div>
+          ))}
 
-            </div>
-            </div>
-            <div className="col-md-2">
-            <div className="card text-center">
-                <div>
-                    <h1>Card</h1>
-                    <h1>2</h1>
-                </div>
-
-            </div>
-            </div><div className="col-md-2">
-            <div className="card text-center">
-                <div>
-                    <h1>Card</h1>
-                    <h1>3</h1>
-                </div>
-
-            </div>
-            </div><div className="col-md-2">
-            <div className="card text-center">
-                <div>
-                    <h1>Card</h1>
-                    <h1>4</h1>
-                </div>
-
-            </div>
-            </div><div className="col-md-2">
-            <div className="card text-center">
-                <div>
-                    <h1>Card</h1>
-                    <h1>5</h1>
-                </div>
-
-            </div>
-            </div><div className="col-md-2">
-            <div className="card text-center">
-                <div>
-                    <h1>Card</h1>
-                    <h1>6</h1>
-                </div>
-
-            </div>
-            </div>
+          
         </div>
-        
+
         <div className=" py-5">
           <button className="btn btn-primary" onClick={myFunction}>
             {" "}
             Click Me
           </button>
         </div>
-        <div className="row text-center py-5">
-        <div className="col-md-3 card-data">
-        <div className="card m-4 p-2 text-center" style={{width:"200px",height:"150px"}}>
-                <div >
-                    <h1 >Card</h1>
-                    <h1>{data}</h1>
+        <div className="row text-center random-card py-5">
+          <div className="col-md-4"></div>
+        <div className="col-md-4">
+              {" "}
+              <div className="card px-2 py-3" style={{}}>
+                {" "}
+                <div className="random-img">
+
+                <img src={imag} className="text-center  img-fluid" style={{width:"200px"}} />{" "}
                 </div>
-
-        </div>
-        </div>
-
+                <div className="card-body">
+                  {" "}
+                  <h1>{data}</h1>
+                  <h5 className="card-title">Card {id}</h5>{" "}
+                  {/* <p className="card-text">{items.text}</p>{" "} */}
+                </div>{" "}
+              </div>{" "}
+            </div>
         </div>
       </div>
     </div>
